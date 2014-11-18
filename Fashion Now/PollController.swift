9bab513a0e34e6fc3c92dec733c3c8600a92e1df
Igor Camilo <obitow@MacBook-Pro-de-Igor.local>
2014-11-17 23:13:06 -0200

    Vote animation completed

--- Fashion Now/PollController.swift
+++ Fashion Now/PollController.swift
@@ -22,61 +22,85 @@ internal class PollController: UIViewController {
     private(set) var leftPhotoController: PhotoController!
     private(set) var rightPhotoController: PhotoController!
 
+    var photoViews: [UIView]!
     @IBOutlet var leftPhotoView: UIView!
     @IBOutlet var rightPhotoView: UIView!
-    
-    var masked = false
 
     @IBAction func didDrag(sender: UIPanGestureRecognizer) {
-        
-        switch sender.state {
-        case .Possible: // the recognizer has not yet recognized its gesture, but may be evaluating touch events. this is the default state
-            break
-        case .Began:
-            for photoView in [leftPhotoView, rightPhotoView] {
-                photoView.layer.zPosition = 0
+
+        func setMaskTranslateX(translate: CGFloat, #view: UIView, #duration: CFTimeInterval?) {
+            CATransaction.begin()
+            if let unwrappedDuration = duration {
+                CATransaction.setAnimationDuration(unwrappedDuration)
+            } else {
+                CATransaction.setDisableActions(true)
             }
-            sender.view?.layer.zPosition = 1
-        case .Changed: // the recognizer has received touches recognized as a change to the gesture. the action method will be called at the next turn of the run loop
-            var photoViewFrame = sender.view!.frame
-            photoViewFrame.origin.x += sender.translationInView(view).x / 2
-            sender.view!.frame = photoViewFrame
+
+            var layerMaskTransform = view.layer.mask.transform
+            layerMaskTransform.m41 = translate
+            view.layer.mask.transform = layerMaskTransform
+
+            CATransaction.commit()
+        }
+
+        switch sender.state {
+
+        case .Changed:
+            let translationX = sender.translationInView(view).x
+            let draggingView = (translationX > 0 ? leftPhotoView : rightPhotoView)
+            for photoView in photoViews {
+                let isDraggingView = (photoView == draggingView)
+                photoView.transform = CGAffineTransformMakeTranslation((isDraggingView ? translationX / 2 : 0), 0)
+                setMaskTranslateX(translationX / (isDraggingView ? 2 : 1), view: photoView, duration: nil)
+            }
         case .Ended: fallthrough
         case .Cancelled: fallthrough
         case .Failed:
-            UIView.animateWithDuration(0.15) { () -> Void in
-                self.view.setNeedsLayout()
-                self.view.layoutIfNeeded()
+            for photoView in photoViews {
+                setMaskTranslateX(0, view: photoView, duration: 0.15)
+                UIView.animateWithDuration(0.15) { () -> Void in
+                    photoView.transform = CGAffineTransformIdentity
+                }
+            }
+        default:
+            return
+        }
+
+
+    }
+
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+
+        if let identifier = segue.identifier {
+            switch identifier {
+
+            case "Left Photo Controller":
+                leftPhotoController = segue.destinationViewController as PhotoController
+            case "Right Photo Controller":
+                rightPhotoController = segue.destinationViewController as PhotoController
+            default:
+                return
             }
         }
-        
-        sender.setTranslation(CGPointZero, inView: view)
     }
-    
-    func adjustMaskSizeWithAnimationDuration(duration: CFTimeInterval?) {
-        
+
+    // MARK: Layout
+
+    func adjustMaskSizeWithAnimationDuration(duration: CFTimeInterval) {
+
         var photoViewSize = view.bounds.size
         photoViewSize.width /= 2
-        
-        if duration != nil {
-            CATransaction.begin()
-            CATransaction.setAnimationDuration(duration!)
-            CATransaction.setAnimationTimingFunction(CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut))
+
+        CATransaction.begin()
+        CATransaction.setAnimationDuration(duration)
+        CATransaction.setAnimationTimingFunction(CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut))
+        for photoView in photoViews {
+            photoView.layer.mask.transform = CATransform3DMakeScale(photoViewSize.width, photoViewSize.height, 1)
         }
-        
-        for photoView in [leftPhotoView, rightPhotoView] {
-            // 2D scale
-            var layerMaskTransform = photoView.layer.mask.transform
-            layerMaskTransform.m11 = photoViewSize.width
-            layerMaskTransform.m22 = photoViewSize.height
-            photoView.layer.mask.transform = layerMaskTransform
-        }
-        
-        if duration != nil {
-            CATransaction.commit()
-        }
+        CATransaction.commit()
     }
-    
+
+    var masked = false
     override func viewWillLayoutSubviews() {
         super.viewWillLayoutSubviews()
         
@@ -85,33 +109,31 @@ internal class PollController: UIViewController {
             let spaceBetween: CGFloat = maskReferenceSize / 100
             
             let rightMaskPath = UIBezierPath()
-            rightMaskPath.moveToPoint(CGPoint(x: maskReferenceSize, y: 0))
+            rightMaskPath.moveToPoint(CGPoint(x: 3 * maskReferenceSize, y: 0))
             rightMaskPath.addLineToPoint(CGPoint(x: (maskReferenceSize / 10) + spaceBetween, y: 0))
             rightMaskPath.addLineToPoint(CGPoint(x: (maskReferenceSize / -10) + spaceBetween, y: maskReferenceSize))
-            rightMaskPath.addLineToPoint(CGPoint(x: maskReferenceSize, y: maskReferenceSize))
+            rightMaskPath.addLineToPoint(CGPoint(x: 3 * maskReferenceSize, y: maskReferenceSize))
             rightMaskPath.closePath()
             let rightMask = CAShapeLayer()
             rightMask.path = rightMaskPath.CGPath
             rightPhotoView.layer.mask = rightMask
             
             let leftMaskPath = UIBezierPath()
-            leftMaskPath.moveToPoint(CGPoint(x: 0, y: 0))
+            leftMaskPath.moveToPoint(CGPoint(x: -2 * maskReferenceSize, y: 0))
             leftMaskPath.addLineToPoint(CGPoint(x: maskReferenceSize + (maskReferenceSize / 10) - spaceBetween, y: 0))
             leftMaskPath.addLineToPoint(CGPoint(x: maskReferenceSize - (maskReferenceSize / 10) - spaceBetween, y: maskReferenceSize))
-            leftMaskPath.addLineToPoint(CGPoint(x: 0, y: maskReferenceSize))
+            leftMaskPath.addLineToPoint(CGPoint(x: -2 * maskReferenceSize, y: maskReferenceSize))
             leftMaskPath.closePath()
             let leftMask = CAShapeLayer()
             leftMask.path = leftMaskPath.CGPath
             leftPhotoView.layer.mask = leftMask
             
             masked = true
-            adjustMaskSizeWithAnimationDuration(nil)
+            adjustMaskSizeWithAnimationDuration(0)
         }
     }
 
-    override func willAnimateRotationToInterfaceOrientation(toInterfaceOrientation: UIInterfaceOrientation, duration: NSTimeInterval) {
-        adjustMaskSizeWithAnimationDuration(duration)
-    }
+    // MARK: Rotation
 
     override func viewWillTransitionToSize(size: CGSize, withTransitionCoordinator coordinator: UIViewControllerTransitionCoordinator) {
         coordinator.animateAlongsideTransition({ (context) -> Void in
@@ -119,21 +141,15 @@ internal class PollController: UIViewController {
         }, completion: nil)
     }
 
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+    override func willAnimateRotationToInterfaceOrientation(toInterfaceOrientation: UIInterfaceOrientation, duration: NSTimeInterval) {
+        adjustMaskSizeWithAnimationDuration(duration)
+    }
 
-        if let identifier = segue.identifier {
+    // MARK: View lifecycle
 
-            switch identifier {
+    override func viewDidLoad() {
+        super.viewDidLoad()
 
-            case "Left Photo Controller":
-                leftPhotoController = segue.destinationViewController as PhotoController
-
-            case "Right Photo Controller":
-                rightPhotoController = segue.destinationViewController as PhotoController
-
-            default:
-                return
-            }
-        }
+        photoViews = [leftPhotoView, rightPhotoView]
     }
 }
