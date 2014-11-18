a2d46adc4836694dd08debb443f2f7bb82d4bec9
Igor Camilo <obitow@iMac-de-Igor.local>
2014-11-18 13:06:39 -0200

    Vote interactive animation improved. Needs vote completion.

--- Fashion Now/PollController.swift
+++ Fashion Now/PollController.swift
@@ -25,48 +25,55 @@ internal class PollController: UIViewController {
     var photoViews: [UIView]!
     @IBOutlet var leftPhotoView: UIView!
     @IBOutlet var rightPhotoView: UIView!
-
+    @IBOutlet var containerView: UIView!
+    
     @IBAction func didDrag(sender: UIPanGestureRecognizer) {
 
-        func setMaskTranslateX(translate: CGFloat, #view: UIView, #duration: CFTimeInterval?) {
-            CATransaction.begin()
-            if let unwrappedDuration = duration {
-                CATransaction.setAnimationDuration(unwrappedDuration)
-            } else {
-                CATransaction.setDisableActions(true)
-            }
-
+        func setMaskTranslateX(translate: CGFloat, #view: UIView) {
             var layerMaskTransform = view.layer.mask.transform
             layerMaskTransform.m41 = translate
             view.layer.mask.transform = layerMaskTransform
-
-            CATransaction.commit()
         }
 
         switch sender.state {
 
         case .Changed:
-            let translationX = sender.translationInView(view).x
+            var translationX = sender.translationInView(containerView).x
+            let rate = translationX / self.containerView.bounds.width
+            if abs(rate) > 0.5 {
+                translationX -= (rate - (rate > 0 ? 0.5 : -0.5)) * containerView.bounds.width * 0.75
+            }
             let draggingView = (translationX > 0 ? leftPhotoView : rightPhotoView)
+            
+            CATransaction.begin()
+            CATransaction.setDisableActions(true)
             for photoView in photoViews {
                 let isDraggingView = (photoView == draggingView)
-                photoView.transform = CGAffineTransformMakeTranslation((isDraggingView ? translationX / 2 : 0), 0)
-                setMaskTranslateX(translationX / (isDraggingView ? 2 : 1), view: photoView, duration: nil)
+                photoView.layer.transform = CATransform3DMakeTranslation((isDraggingView ? translationX / 2 : 0), 0, 0)
+                setMaskTranslateX(translationX * (isDraggingView ? 0.75 : 1.25), view: photoView)
             }
+            CATransaction.commit()
+            
         case .Ended: fallthrough
         case .Cancelled: fallthrough
         case .Failed:
+            CATransaction.begin()
+            CATransaction.setAnimationDuration(0.25)
+            CATransaction.setAnimationTimingFunction(CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut))
             for photoView in photoViews {
-                setMaskTranslateX(0, view: photoView, duration: 0.15)
-                UIView.animateWithDuration(0.15) { () -> Void in
-                    photoView.transform = CGAffineTransformIdentity
-                }
+                let animation = CABasicAnimation(keyPath: "transform")
+                animation.fromValue = NSValue(CATransform3D: photoView.layer.transform)
+                animation.toValue = NSValue(CATransform3D: CATransform3DIdentity)
+                animation.timingFunction = CATransaction.animationTimingFunction()
+                photoView.layer.addAnimation(animation, forKey: "transform")
+                photoView.layer.transform = CATransform3DIdentity
+                setMaskTranslateX(0, view: photoView)
             }
+            CATransaction.commit()
+            
         default:
             return
         }
-
-
     }
 
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
@@ -88,7 +95,7 @@ internal class PollController: UIViewController {
 
     func adjustMaskSizeWithAnimationDuration(duration: CFTimeInterval) {
 
-        var photoViewSize = view.bounds.size
+        var photoViewSize = containerView.bounds.size
         photoViewSize.width /= 2
 
         CATransaction.begin()
@@ -108,29 +115,34 @@ internal class PollController: UIViewController {
             let maskReferenceSize: CGFloat = 1
             let spaceBetween: CGFloat = maskReferenceSize / 100
             
-            let rightMaskPath = UIBezierPath()
-            rightMaskPath.moveToPoint(CGPoint(x: 3 * maskReferenceSize, y: 0))
-            rightMaskPath.addLineToPoint(CGPoint(x: (maskReferenceSize / 10) + spaceBetween, y: 0))
-            rightMaskPath.addLineToPoint(CGPoint(x: (maskReferenceSize / -10) + spaceBetween, y: maskReferenceSize))
-            rightMaskPath.addLineToPoint(CGPoint(x: 3 * maskReferenceSize, y: maskReferenceSize))
-            rightMaskPath.closePath()
-            let rightMask = CAShapeLayer()
-            rightMask.path = rightMaskPath.CGPath
-            rightPhotoView.layer.mask = rightMask
-            
             let leftMaskPath = UIBezierPath()
-            leftMaskPath.moveToPoint(CGPoint(x: -2 * maskReferenceSize, y: 0))
+            leftMaskPath.moveToPoint(CGPoint(x: -6 * maskReferenceSize, y: 0))
             leftMaskPath.addLineToPoint(CGPoint(x: maskReferenceSize + (maskReferenceSize / 10) - spaceBetween, y: 0))
             leftMaskPath.addLineToPoint(CGPoint(x: maskReferenceSize - (maskReferenceSize / 10) - spaceBetween, y: maskReferenceSize))
-            leftMaskPath.addLineToPoint(CGPoint(x: -2 * maskReferenceSize, y: maskReferenceSize))
+            leftMaskPath.addLineToPoint(CGPoint(x: -6 * maskReferenceSize, y: maskReferenceSize))
             leftMaskPath.closePath()
             let leftMask = CAShapeLayer()
             leftMask.path = leftMaskPath.CGPath
             leftPhotoView.layer.mask = leftMask
             
+            let rightMaskPath = UIBezierPath()
+            rightMaskPath.moveToPoint(CGPoint(x: 7 * maskReferenceSize, y: 0))
+            rightMaskPath.addLineToPoint(CGPoint(x: (maskReferenceSize / 10) + spaceBetween, y: 0))
+            rightMaskPath.addLineToPoint(CGPoint(x: (maskReferenceSize / -10) + spaceBetween, y: maskReferenceSize))
+            rightMaskPath.addLineToPoint(CGPoint(x: 7 * maskReferenceSize, y: maskReferenceSize))
+            rightMaskPath.closePath()
+            let rightMask = CAShapeLayer()
+            rightMask.path = rightMaskPath.CGPath
+            rightPhotoView.layer.mask = rightMask
+            
             masked = true
-            adjustMaskSizeWithAnimationDuration(0)
         }
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        adjustMaskSizeWithAnimationDuration(0)
     }
 
     // MARK: Rotation
